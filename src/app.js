7541d5dddb133817f6607d90994d5b136af6e0a9
Aman Vishwakarma <darkestnight9029@gmail.com>
2025-11-11 00:40:10 +0530

    Add app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.use((req,res)=>{
     res.send("hello ji ");
-})
+});
+app.use("/hello",(req,res)=>{
+    res.send("hello ji ");
+});
 
 app.listen(3000, ()=>{
     console.log("the server is runnning on port 3000....");
